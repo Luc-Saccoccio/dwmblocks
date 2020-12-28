ca142b24d1fe2c232432238f11374b04ae103a79
Luc-Saccoccio <lucsaccoccio@disroot.org>
2020-12-28 14:31:53 +0100

    Added dwmblocks

---
+++ README.md
@@ -0,0 +1,7 @@
+# dwmblocks
+
+This is my dwmblocks build. It won't work unless you use the right scripts (see my dotfiles).
+
+## Installation
+
+To use dwmblocks first run `make` and then install it with `make install`.
---
+++ blocks.def.h
@@ -0,0 +1,19 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"",	"music",		0,	11}, */
+	/* {"",	"pacpackages",		1,	8}, */
+	/* {"",	"news",			0,	6}, */
+	/* {"",	"torrent",		20,	7}, */
+	{"",	"memory",		10,	14},
+	{"",	"cpu",			10,	18},
+	{"",	"mailbox",		180,	12},
+	{"",	"volume",		0,	10},
+	{"",	"battery",		5,	3},
+	{"",	"date '+%D at %H:%M'",	60,	1},
+	{"",	"internet",		5,	4},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
